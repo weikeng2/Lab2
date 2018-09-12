0481f7fdbf0df4ebc34921ce45b1b28e7f64fabf
weikeng2 <42951918+weikeng2@users.noreply.github.com>
2018-09-12 18:09:41 -0500

    Finish?

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,19 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            if (!maze.isFinished()) {
+                maze.turnRight();
+                if (!maze.canMove()) {
+                    maze.turnLeft();
+                }
+                if (!maze.canMove()) {
+                    maze.turnLeft();
+                }
+                if (!maze.canMove()) {
+                    maze.turnLeft();
+                }
+                maze.move();
+            }
         }
 
         if (maze.isFinished()) {
